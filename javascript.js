ccffa06e2913f0fd163a2b9fdb5581acad6ea69b
Majasb123 <majasb08@icloud.com>
2026-05-20 11:59:00 +0200

    ghjgbhkgbk

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 const buttons = document.querySelectorAll(".more")
 
-for ( const button of buttons){
-    button.addEventListener("click", function(){
+for (const button of buttons) {
+    button.addEventListener("click", function () {
         const card = button.closest(".card")
 
         card.classList.toggle("active")
         button.classList.toggle("active")
 
-        if (card.classList.contains("active")){
+        if (card.classList.contains("active")) {
             button.textContent = "lukk"
-        } else{
+        } else {
             button.textContent = "les mer"
         }
 
@@ -31,3 +31,43 @@ x.addEventListener("click", function () {
     x.classList.remove("active")
     navLinks.classList.remove("active")
 })
+
+
+const customerBtn = document.getElementById("customer")
+
+if (customerBtn) {
+
+    customerBtn.addEventListener("click", function () {
+        const navn = document.querySelector(".navn").value;
+        if (navn == "") {
+            alert("Fyll inn informasjon")
+        } else {
+            alert("Gratulerer " + navn + " med medlemskap!")
+        }
+    })
+}
+
+
+const sun = document.querySelector(".sun")
+const moon = document.querySelector(".moon")
+const body = document.querySelector("body")
+
+if (sun && moon) {
+
+    // nattmodus
+    moon.addEventListener("click", function () {
+        body.classList.add("active")
+
+        moon.style.display = "none"
+        sun.style.display = "block"
+    })
+
+    // dagmodus
+    sun.addEventListener("click", function () {
+        body.classList.remove("active")
+
+        sun.style.display = "none"
+        moon.style.display = "block"
+    })
+
+}
